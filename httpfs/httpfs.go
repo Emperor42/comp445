62db44f62001e9cd8edb7a4a86dd64e6dbf0836f
Matthew Salvatore Giancola <matthew.giancola42@gmail.com>
2022-04-10 17:16:58 -0400

    Completion of requirements for Lab 3

--- httpfs/httpfs.go
+++ httpfs/httpfs.go
@@ -1,50 +1,49 @@
 package main
 
-
 import (
-	"net"
 	"bufio"
+	"comp445/udp"
 	"fmt"
-	"os"
-    "path/filepath"
-	"strings"
-	"io/ioutil"
 	"io"
+	"io/ioutil"
+	"net"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
 	//"comp445/httpc/http"
 )
 
-
-
 func handleConnection(conn net.Conn, path string, verbose bool) {
 	defer conn.Close()
-	//read in data from the connection in a loop, using boolean values to determine what to do 
+	//read in data from the connection in a loop, using boolean values to determine what to do
 	opset := false
 	opGET := false
 	opPOST := false
 	filePath := ""
 	opLIST := false
 	//headers have been processed
-	headers:=true
+	headers := true
 	//data for file system
-	fileData:=""
+	fileData := ""
 	//start loop
-	connection := bufio.NewReader(conn);
+	connection := bufio.NewReader(conn)
 	//out of loop
-	readData:=""
+	readData := ""
 	//var err error
-	
-	for status, err := connection.ReadString('\n');(err==nil&&status!="\n"&&status!="\r");status, err = connection.ReadString('\n') {
-		if connection.Buffered()<4 {
+
+	for status, err := connection.ReadString('\n'); err == nil && status != "\n" && status != "\r"; status, err = connection.ReadString('\n') {
+		if connection.Buffered() < 4 {
 			break
 		}
-		fmt.Println(strings.ReplaceAll(status, "\r",";"))
+		fmt.Println(strings.ReplaceAll(status, "\r", ";"))
 		fmt.Println(err)
 		fmt.Println(status)
 		fmt.Println(connection.Buffered())
 		//do stuff
 		if opset {
 			//check if we have been processing the headers
-			if status == "\r"{
+			if status == "\r" {
 				//we are exiting a section
 				if headers {
 					headers = false
@@ -56,7 +55,7 @@ func handleConnection(conn net.Conn, path string, verbose bool) {
 				//TODO: Process headers
 			} else {
 				//append file data
-				fileData = fileData+status
+				fileData = fileData + status
 			}
 			//do stuff
 		} else {
@@ -65,38 +64,38 @@ func handleConnection(conn net.Conn, path string, verbose bool) {
 			opGET = false
 			opPOST = false
 			opLIST = false
-			pathSET:=false
+			pathSET := false
 			//read data
-			tmp:=strings.Split(status, " ")
-			for i:=0;i<len(tmp);i++ {
-				if tmp[i]=="GET" {
-					opGET=true
-					opset=true
+			tmp := strings.Split(status, " ")
+			for i := 0; i < len(tmp); i++ {
+				if tmp[i] == "GET" {
+					opGET = true
+					opset = true
 					continue
 				}
-				if tmp[i]=="POST" {
-					opPOST=true
-					opset=true
+				if tmp[i] == "POST" {
+					opPOST = true
+					opset = true
 					continue
 				}
-				//error check 
+				//error check
 				if opPOST && opGET {
 					panic("Incorrect Format")
 				}
 				// check if the value is the endline if so then exit the loop
-				if tmp[i]=="HTTP/1.0\r" {
+				if tmp[i] == "HTTP/1.0\r" {
 					break
 				}
 				//check if its root
-				if tmp[i]=="/" {
+				if tmp[i] == "/" {
 					opLIST = true
 					opGET = false
-					opset=true
+					opset = true
 					continue
 				}
 				//finally one can assume that this is a path to be used (omit the first /)
-				if len(tmp[i])>1{
-					if strings.Contains(tmp[i], "/") && !pathSET{
+				if len(tmp[i]) > 1 {
+					if strings.Contains(tmp[i], "/") && !pathSET {
 						filePath = tmp[i][1:]
 						pathSET = true
 					}
@@ -107,13 +106,13 @@ func handleConnection(conn net.Conn, path string, verbose bool) {
 			}
 		}
 		if verbose {
-			fmt.Println(">"+status+"<")
+			fmt.Println(">" + status + "<")
 		}
 	}
 	var buf []byte
 	flush, err := io.ReadFull(connection, buf)
 	fmt.Println(flush)
-	readData = readData+string(buf)
+	readData = readData + string(buf)
 	//we execute the functions generated
 	fmt.Println("Rrequest Reading Complete")
 	fmt.Print("opset: ")
@@ -126,29 +125,29 @@ func handleConnection(conn net.Conn, path string, verbose bool) {
 	fmt.Println(opLIST)
 	if opset {
 		//clean the path (***SECURITY MECHANISM***)
-		filePath = path+"/"+strings.ReplaceAll(filePath, "/", "")
+		filePath = path + "/" + strings.ReplaceAll(filePath, "/", "")
 		fmt.Println(filePath)
-		if opLIST{
+		if opLIST {
 			//list files in working directory
 			fmt.Println("OPLIST")
 			info, erra := os.ReadDir(path)
 			fmt.Println(erra)
-			for i:=0;i<len(info);i++ {
+			for i := 0; i < len(info); i++ {
 				fmt.Println(info[i].Name())
-				readData = readData+info[i].Name()
+				readData = readData + info[i].Name()
 			}
-			
+
 		} else if opGET {
 			fmt.Println("OPGET")
 			//read from a file found
 			tmpData, errb := ioutil.ReadFile(filePath)
-			if errb==nil {
+			if errb == nil {
 				fmt.Println(string(readData))
 				readData = string(tmpData)
-			}else {
+			} else {
 				fmt.Println(errb)
 			}
-		} else if opPOST{
+		} else if opPOST {
 			fmt.Println("OPPOST")
 			//write to a file found
 			msg := []byte(fileData)
@@ -159,61 +158,56 @@ func handleConnection(conn net.Conn, path string, verbose bool) {
 		} else {
 			panic("THERE WAS NO CORRECT OPERATION SET!")
 		}
-	}else {
+	} else {
 		panic("THERE WAS NO CORRECT OPERATION SET!")
 	}
 	resp := "404"
-	s :=[]byte(resp+readData)
+	s := []byte(resp + readData)
 	//http.QuickConnect(verbose, s, conn)
-	dataList, err:=conn.Write(s)
-	if err==nil{
+	dataList, err := conn.Write(s)
+	if err == nil {
 		fmt.Println(dataList)
-	}else {
+	} else {
 		fmt.Println(err)
 	}
 	//the connection should close
 }
 
-
 func main() {
 	//for the directory default
 	ex, err := os.Executable()
-    if err != nil {
-        panic(err)
-    }
-    path := filepath.Dir(ex)
-    verbose := false
-	port :="8080"
+	if err != nil {
+		panic(err)
+	}
+	path := filepath.Dir(ex)
+	verbose := false
+	port := "8080"
 	//start processing the arguments
 	args := os.Args[1:]
-	for i:=0;i<len(args);i++ {
-		switch args[i]{
+	for i := 0; i < len(args); i++ {
+		switch args[i] {
 		case "-v":
 			verbose = true
 		case "-p":
 			i++
-			if i==len(args){
+			if i == len(args) {
 				panic("Out of Bounds!")
 			}
 			port = args[i]
 		case "-d":
 			i++
-			if i==len(args){
+			if i == len(args) {
 				panic("Out of Bounds!")
 			}
 			path = args[i]
 		}
 	}
 	//all values set
-	fmt.Println("Running on :"+port )
-	ln, err := net.Listen("tcp", ":"+port)
-	if err != nil {
-		if verbose {
-			fmt.Println(err)
-		}
-	}
+	fmt.Println("Running on :" + port)
+	portNumber, _ := strconv.Atoi(port)
+	udp := udp.Server("udp", portNumber, 10000)
 	for {
-		conn, err := ln.Accept()
+		conn, err := udp.Handshake()
 		if err != nil {
 			if verbose {
 				fmt.Println(err)
--- udp/udp.go
+++ udp/udp.go
@@ -1,6 +1,7 @@
-package main
+package udp
 
 import (
+	"fmt"
 	"net"
 	"strconv"
 	"strings"
@@ -32,22 +33,53 @@ func generatePacket(packetType byte, seqNum uint32, host string, port uint16, pa
 	p.data[8] = byte(d)
 	p.data[9] = byte(port / 256)
 	p.data[10] = byte(port % 256)
-	for i := 11; i < len(payload); i++ {
-		p.data[i] = byte(payload[i])
+	if payload != "" {
+		for i := 11; i < len(payload); i++ {
+			p.data[i] = byte(payload[i])
+		}
+	}
+	return p
+}
+
+func (u *UDP) generatePacket(packetType byte, seqNum uint32, payload string) Packet {
+	if len(payload) > 1013 {
+		panic("Packet payload is too large!")
+	}
+	var p Packet
+	p.data = make([]byte, 1024)
+	p.data[0] = packetType
+	p.data[1] = 0
+	p.data[2] = 0
+	p.data[3] = byte(seqNum / 256)
+	p.data[4] = byte(seqNum % 256)
+	a := u.addr.IP.To4()
+	port := (uint16(u.addr.Port))
+	if a != nil {
+		p.data[5] = a[0]
+		p.data[6] = a[1]
+		p.data[7] = a[2]
+		p.data[8] = a[3]
+	}
+	p.data[9] = byte(port / 256)
+	p.data[10] = byte(port % 256)
+	if payload != "" {
+		for i := 11; i < len(payload); i++ {
+			p.data[i] = byte(payload[i])
+		}
 	}
 	return p
 }
 
 //packetType byte, seqNum uint32, host string, port uint16, payload string
-func (p Packet) PacketType() byte {
+func (p *Packet) PacketType() byte {
 	return p.data[0]
 }
 
-func (p Packet) PacketNumber() uint32 {
+func (p *Packet) PacketNumber() uint32 {
 	return (uint32(p.data[3]) * 256) + uint32(p.data[4])
 }
 
-func (p Packet) PeerAddress() string {
+func (p *Packet) PeerAddress() string {
 	a := strconv.Itoa(int(p.data[5]))
 	b := strconv.Itoa(int(p.data[6]))
 	c := strconv.Itoa(int(p.data[7]))
@@ -55,11 +87,11 @@ func (p Packet) PeerAddress() string {
 	return a + "." + b + "." + "." + c + "." + d
 }
 
-func (p Packet) PeerPort() uint16 {
+func (p *Packet) PeerPort() uint16 {
 	return (uint16(p.data[9]) * 256) + uint16(p.data[10])
 }
 
-func (p Packet) Payload() string {
+func (p *Packet) Payload() string {
 	ret := ""
 	for i := 11; i < 1024; i++ {
 		ret = ret + string(p.data[i])
@@ -67,20 +99,196 @@ func (p Packet) Payload() string {
 	return ret
 }
 
-type UDP struct {
-	conn *net.UDPConn
-	addr *net.UDPAddr
+func (p *Packet) Data() bool {
+	if p.data != nil && len(p.data) > 0 {
+		return p.data[0] == 0
+	}
+	return false
 }
 
-func Client(network string) *UDP {
+func (p *Packet) SYN() bool {
+	if p.data != nil && len(p.data) > 0 {
+		return p.data[0] == 1
+	}
+	return false
+}
+
+func (p *Packet) SYNACK() bool {
+	if p.data != nil && len(p.data) > 0 {
+		return p.data[0] == 2
+	}
+	return false
+}
+
+func (p *Packet) ACK() bool {
+	if p.data != nil && len(p.data) > 0 {
+		return p.data[0] == 3
+	}
+	return false
+}
+
+func (p *Packet) NAK() bool {
+	if p.data != nil && len(p.data) > 0 {
+		return p.data[0] == 4
+	}
+	return false
+}
+
+type UDP struct {
+	conn      *net.UDPConn
+	lastRead  int
+	status    error
+	addr      *net.UDPAddr
+	client    bool
+	buffer    [1024](*Packet)
+	index     int
+	connected bool
+	timeout   int
+	sent      int
+	network   string
+	port      int
+}
+
+func (u *UDP) Buffer() [1024](*Packet) {
+	u.process()
+	return u.buffer
+}
+
+func (u *UDP) process() {
+	offset := -1
+	//find minimum sequence number, that is our zero
+	for i := 0; i < 1024; i++ {
+		p := u.buffer[i]
+		if p != nil {
+			num := int(p.PacketNumber())
+			//find min seqeuence number
+			if offset == -1 {
+				offset = num
+			} else {
+				if offset < num {
+					offset = num
+				}
+			}
+		}
+	}
+	//assign packets to correct locations
+	var tempBuffer [1024](*Packet)
+	for i := 0; i < 1024; i++ {
+		p := u.buffer[i]
+		if p != nil {
+			num := int(p.PacketNumber()) - offset
+			//find min seqeuence number
+			tempBuffer[num] = p
+		}
+	}
+	u.buffer = tempBuffer
+}
+
+func (u *UDP) includePacket(p *Packet) {
+	if p != nil {
+		u.buffer[u.index%1024] = p
+		u.index++
+	}
+}
+
+func (u *UDP) Connection() *net.UDPConn {
+	return u.conn
+}
+
+func (u *UDP) Handshake() (*net.UDPConn, error) {
+	synPacket := u.generatePacket(1, uint32(u.sent), "")
+	synackPacket := u.generatePacket(2, uint32(u.sent), "")
+	ackPacket := u.generatePacket(3, uint32(u.sent), "")
+	if u.client {
+		u.Write(&synPacket)
+		for i := 0; i < u.timeout; i++ {
+			var temp Packet
+			p := &temp
+			u.Read(p)
+			u.includePacket(p)
+			var ack bool
+			for i := 0; i < 1024; i++ {
+				p := u.buffer[i]
+				if p != nil {
+					if !ack {
+						ack = p.SYNACK()
+					}
+				}
+			}
+			if ack {
+				u.connected = true
+				u.Write(&ackPacket)
+				break
+			}
+		}
+	} else {
+		for i := 0; i < u.timeout; i++ {
+			var temp Packet
+			p := &temp
+			u.Read(p)
+			u.includePacket(p)
+			var syn, ack bool
+			for i := 0; i < 1024; i++ {
+				p := u.buffer[i]
+				if p != nil {
+					if !syn {
+						syn = p.SYN()
+					}
+					if !ack {
+						ack = p.ACK()
+					}
+				}
+			}
+			if syn && ack {
+				u.connected = true
+				break
+			} else if syn && !ack {
+				u.Write(&synackPacket)
+			}
+		}
+	}
+	return u.conn, nil
+}
+
+func (u *UDP) Read(p *Packet) *net.UDPAddr {
+	u.lastRead, u.addr, u.status = u.conn.ReadFromUDP(p.data)
+	if u.status == nil {
+		u.process()
+		return u.addr
+	}
+	return nil
+}
+
+func (u *UDP) Write(p *Packet) *net.UDPAddr {
+	u.lastRead, u.status = u.conn.WriteToUDP(p.data, u.addr)
+	if u.status == nil {
+		u.process()
+		return u.addr
+	}
+	return nil
+}
+
+func Client(network string, port, timeout int) *UDP {
 	var u UDP
-	u.conn, _ = net.DialUDP(network, nil, nil)
+	u.client = true
+	u.network = network
+	u.port = port
+	u.connected = false
+	u.timeout = timeout
+	u.addr, u.status = net.ResolveUDPAddr(network, fmt.Sprintf(":%d", port))
+	u.conn, u.status = net.ListenUDP(network, u.addr)
 	return &u
 }
 
-func Server(network string) *UDP {
+func Server(network string, port, timeout int) *UDP {
 	var u UDP
-	u.conn, _ = net.ListenUDP(network, nil)
+	u.client = true
+	u.network = network
+	u.port = port
+	u.connected = false
+	u.timeout = timeout
+	u.addr, u.status = net.ResolveUDPAddr(network, fmt.Sprintf(":%d", port))
+	u.conn, u.status = net.ListenUDP(network, u.addr)
 	return &u
 }
 
